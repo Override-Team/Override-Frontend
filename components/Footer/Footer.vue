32f59f5712f5df3d5e0d0880585d5f3a5cff3471
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-07-20 11:35:47 +0200

    Why overrite?

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -31,7 +31,7 @@
       </UContainer>
       <UContainer class="py-4 gap-2 ">
         <UDivider class="my-4"/>
-        <h1>Overrite</h1>
+        <h1>Override</h1>
       </UContainer>
     </footer>
   </div>
