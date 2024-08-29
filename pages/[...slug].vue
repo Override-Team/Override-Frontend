af86ecef622fe31d7ca33967c705b3e6dea9c015
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-08-29 22:34:18 +0200

    Changes to homepage - removed HomepageFeatures.vue - MainCatPages.vue schedules for deletion soon - added 2 buttons under text

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import HomepageFeatures from "~/components/Homepage/HomepageFeatures.vue";
 import MainCatPages from "~/components/Homepage/MainCatPages.vue";
 </script>
 
 <template>
   <section>
     <div class="max-w-screen-xl mx-auto px-4 py-28 gap-12 md:px-8">
-      <div  data-aos="fade-up">
-        <div class="space-y-5 max-w-4xl mx-auto text-center">
+      <div data-aos="fade-up">
+        <div class="py-1 space-y-5 max-w-4xl mx-auto text-center">
           <h1>Override</h1>
         </div>
         <p class="dark:text-white">Upload your plugins and schematics fast and easy</p>
       </div>
+      <div class="py-3 space-x-4 space-y-5 max-w-4xl mx-auto text-center">
+        <UButton to="/plugins" label="Browse Plugins" leading-icon="i-heroicons-command-line" variant="outline" color="gray" size="md"/>
+        <UButton to="/account/signup" label="Sign Up" leading-icon="i-heroicons-user-16-solid" variant="solid" color="primary" size="md"/>
+      </div>
       <div class="max-w-screen-xl mx-auto px-4 py-28 gap-12 md:px-8">
         <MainCatPages />
-        <div class="py-20">
-          <HomepageFeatures />
-        </div>
       </div>
     </div>
   </section>
