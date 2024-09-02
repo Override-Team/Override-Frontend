3772cfe456e44af22e55a572c65a34acf9233361
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-09-03 00:44:14 +0200

    Small change to filter

--- components/Search/FilterPlugins.vue
+++ components/Search/FilterPlugins.vue
@@ -12,6 +12,7 @@ const selected = ref()
       <UCheckbox color="primary" label="Exiled"/>
       <UCheckbox color="primary" label="NW API" :model-value="false" />
     </div>
+    <UDivider class="py-4"/>
     <UCard class="flex icon-and-text" style="width: 400px; height: 40px">
       <h1>Categories</h1>
     </UCard>
@@ -24,6 +25,7 @@ const selected = ref()
       <UCheckbox color="primary" label="RolePlay" :model-value="false" />
       <UCheckbox color="primary" label="Custom Mapping" :model-value="false"/>
     </div>
+    <UDivider class="py-4"/>
     <UCard class="flex icon-and-text" style="width: 400px; height: 40px">
       <h1>Game Version</h1>
     </UCard>
@@ -36,6 +38,7 @@ const selected = ref()
           :options="['13.5.1 (LATEST)', '13.5', '13.4.2', '13.4.1', '13.4.0', '13.3.1', '13.3.0', '13.2.1', '13.2.0', '13.1.1', '13.1.0', '13.0.0', '12.0.2', '12.0.1', '12.0.0', '11.2.1', '11.2.0', '11.1.5', '11.1.4', '11.1.3', '11.1.2', '11.1.1', '11.1.0', '11.0.3', '11.0.2', '11.0.1', '11.0.0', '10.2.2', '10.2.1', '10.2.0', '10.1.3', '10.1.2', '10.1.1', '10.1.0', '10.0.6', '10.0.5', '10.0.4', '10.0.3', '10.0.2', '10.0.1', '10.0.0', '9.1.3', '9.1.1', '9.1.0', '9.0.3', '9.0.2', '9.0.1', '9.0.0', '8.0.0', '7.4.0', '7.3.1', '7.3.0', '7.2.0', '7.1.0', '7.0.0', '6.2.0', '6.0.1', '6.0.0', '5.1.1', '5.1.0', '5.0.1', '5.0.0', '4.0.0', '3.3.3', '3.3.2', '3.3.1', '3.3.0', '3.1.0', '3.0.0', '1.1.0', '1.0.2', '1.0.0', '0.4.2', '0.4.1', '0.3.2', '0.3.1', '0.3.0', '0.2.1+0.2.1b', '0.2.0', ]"
       />
     </div>
+    <UDivider class="py-4"/>
     <UCard class="flex icon-and-text" style="width: 400px; height: 40px">
       <h1>Other</h1>
     </UCard>
