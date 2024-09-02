b05cf0506a99be3ccac7c27ada6116d5cd140e6e
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-09-02 23:19:26 +0200

    WIP about us page

---
+++ pages/about-us/[...slug].vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UContainer class="py-10">
+    <div class="">
+      <UCard style="background-color: #121212; width: 550px;">
+      <h1>What exactly is Override?</h1>
+      <p>
+        Override is a platform for hosting your SCP:SL plugins or schematics. They are also made more accessibly for users to find the right version
+        and then download it
+      </p>
+      </UCard>
+    </div>
+  </UContainer>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 30px;
+  font-family: "Inter", sans-serif;
+  font-weight: bold;
+  text-align: left;
+  color: white;
+}
+</style>
